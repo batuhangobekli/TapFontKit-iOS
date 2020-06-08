de8709a4d2100a9ff558d5035c485b4d523486d8
O Rabie <o.rabie@tap.company>
2020-06-08 10:21:04 +0200

    1- removed all non needed kits. 2- Added Roboto family fonts. 3- Updated font rgistering code to swift 5. 4- Added example project

--- TapFontKit-iOS/TapFontKitExample/ViewController.swift
+++ TapFontKit-iOS/TapFontKitExample/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import TapFontKit_iOS
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var enLabel:UILabel!
+    @IBOutlet weak var arLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        enLabel.font = FontProvider.localizedFont(.robotoLight, size: 20, languageIdentifier: "en")
+        
+        arLabel.font = FontProvider.localizedFont(.robotoLight, size: 20, languageIdentifier: "ar")
     }
-
-
+    
+    
 }
 
