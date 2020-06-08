ae2f8408668168b5244234a3b16a3a978e3f20bf
O Rabie <o.rabie@tap.company>
2020-06-08 09:24:09 +0200

    1- Created structure for tap font kit. 2- Added example project. 3- Added needed libraries

---
+++ TapFontKit-iOS/Pods/Target Support Files/Pods-TapFontKit-iOS-TapFontKit-iOSTests/Pods-TapFontKit-iOS-TapFontKit-iOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TapFontKit_iOS_TapFontKit_iOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_TapFontKit_iOS_TapFontKit_iOSTests
+@end
